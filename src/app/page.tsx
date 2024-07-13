e7fa6d58ee9b2c38a008287d13c3874fd03aa49a
DMaximoVarela <maxivarela95@gmail.com>
2024-07-13 10:33:17 -0300

    feat: añade un fondo azul gradiente radial en la landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
+import style from "./page.module.css";
+
 export default function Home() {
   return (
     <main>
+      <div className={style.bg}></div>
       <div>
         <h1>CSS Magic Buttons</h1>
       </div>
