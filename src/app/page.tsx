66ab85abc59363b9ab2755c91c664d011e0d695c
DMaximoVarela <maxivarela95@gmail.com>
2024-07-14 08:29:01 -0300

    feat: añade el texto central en la landing page y lo diseña responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,26 @@
 import style from "./page.module.css";
+import { FaArrowRight } from "react-icons/fa";
 
 export default function Home() {
   return (
-    <main>
+    <main className="flex flex-col justify-center items-center">
       <div className={style.bg}></div>
-      <div>
-        <h1>CSS Magic Buttons</h1>
+      <div className="text-center px-6">
+        <h1 className="text-[32px] sm:text-[42px] lg:text-[52px] 2xl:text-[72px] 4xl:text-[84px] font-bold">
+          Descubre <span className="text-[#4A90E2]">+100 Botones</span>{" "}
+          <span className="sm:hidden">con</span>
+          <br />
+          <span className="xsm:hidden sm:inline-block">con </span>{" "}
+          <span className="text-[#4A90E2] inline-block">Código CSS</span>
+        </h1>
+        <h3 className="text-[16px] sm:text-[24px] xl:text-[32px] 4xl:text-[36px] font-normal text-[#BDC3C7]">
+          Copia el código CSS de cualquier <br className="lg:hidden" /> botón
+          con solo un clic.
+        </h3>
+      </div>
+      <div className="h-[32px] sm:h-[40px] lg:h-[50px] 2xl:h-[64px] my-4 flex justify-center items-center text-[16px] sm:text-[24px] xl:text-[32px] font-medium px-4 text-[#4a90e2] border border-[#4a90e2] rounded-3xl bg-[#1456a44d] hover:bg-[#1456a499] cursor-pointer transition duration-500 ease-in-out transform">
+        <span>Iniciar Viaje</span>
+        <FaArrowRight />
       </div>
     </main>
   );
