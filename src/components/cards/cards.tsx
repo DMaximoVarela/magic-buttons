9fba6e7a4f09d6c8929005325d43c3a18fdcecd6
DMaximoVarela <maxivarela95@gmail.com>
2024-08-12 22:58:22 -0300

    feat: crea la store para los botones junto a su getButtons

--- src/components/cards/cards.tsx
+++ src/components/cards/cards.tsx
@@ -1,8 +1,23 @@
+import useFiltersStore from "@/stores/filtersStore";
 import ButtonContainer from "../buttonContainer/buttonContainer";
 import buttons from "../buttons/buttons";
+import { useEffect } from "react";
+import useButtonsStore from "@/stores/buttonsStore";
 
 const Cards = () => {
   const copyButtons = buttons.slice(1);
+
+  const filters = useFiltersStore((state) => state.filters);
+  const getButtons = useButtonsStore((state) => state.getButtons);
+  const allButtons = useButtonsStore((state) => state.buttons);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      await getButtons();
+    };
+
+    fetchData();
+  }, [filters]);
 
   return (
     <div>
---
+++ src/stores/buttonsStore.ts
@@ -0,0 +1,58 @@
+import { create } from "zustand";
+import axios from "axios";
+
+const API_URL = process.env.NEXT_PUBLIC_API_URL;
+
+interface Button {
+  id: number;
+  categoria: string;
+  tamano: string;
+  estilo: string;
+  htmlCode: string;
+  cssCode: string;
+  tailwindCode: string;
+}
+
+interface State {
+  buttons: Button[];
+  getButtons: (categoria?: string, tamano?: string, estilo?: string) => void;
+}
+
+const getButtons = async (
+  categoria?: string,
+  tamano?: string,
+  estilo?: string
+) => {
+  try {
+    let url = `${API_URL}buttons`;
+    const params = new URLSearchParams();
+
+    if (categoria) params.append("categoria", categoria);
+    if (tamano) params.append("tamano", tamano);
+    if (estilo) params.append("estilo", estilo);
+
+    if (params.toString()) {
+      url += `?${params.toString()}`;
+    }
+
+    const response = await axios.get(url);
+    return response.data;
+  } catch (error) {
+    console.error("Error al obtener botones:", error);
+    throw error;
+  }
+};
+
+const useButtonsStore = create<State>((set) => ({
+  buttons: [],
+  getButtons: async (categoria?: string, tamano?: string, estilo?: string) => {
+    try {
+      const buttonsFiltered = await getButtons(categoria, tamano, estilo);
+      set({ buttons: buttonsFiltered });
+    } catch (error) {
+      console.error("Error en el store:", error);
+    }
+  },
+}));
+
+export default useButtonsStore;
