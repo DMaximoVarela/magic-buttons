b69bfda8b32953e277cff1a8ba8b23196c61ed02
DMaximoVarela <maxivarela95@gmail.com>
2024-08-30 21:00:13 -0300

    feat: add magic touch functionality

--- src/components/navMobile/nav.tsx
+++ src/components/navMobile/nav.tsx
@@ -1,16 +1,53 @@
+"use client";
+
+import { useEffect, useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { IoIosSearch } from "react-icons/io";
 import { FaWandMagicSparkles } from "react-icons/fa6";
 import { IoLanguageSharp } from "react-icons/io5";
 import { TiArrowSortedDown } from "react-icons/ti";
+import useButtonsStore from "@/stores/buttonsStore";
+import Modal from "../modal/modal";
 
 const NavMobile = () => {
+  const [count, setCount] = useState(0);
+  const [modal, setModal] = useState(false);
+
+  const getRandomButton = useButtonsStore((state) => state.getRandomButton);
+  const button = useButtonsStore((state) => state.randomButton);
+
+  let buttonId = Number(button?.id);
+
+  const generateRandomNumber = (max: number, min: number) => {
+    return Math.round(Math.random() * (max - min) + min);
+  };
+
+  const onClick = async () => {
+    setCount(count + 1);
+
+    if (button) {
+      setModal(true);
+    } else {
+      alert(`Se produjo un error desconocido 💀`);
+    }
+  };
+
+  useEffect(() => {
+    const randomNumber = generateRandomNumber(1, 6);
+
+    const fetchData = async () => {
+      await getRandomButton(randomNumber.toString());
+    };
+
+    fetchData();
+  }, [count]);
+
   return (
-    <div className="w-full h-[48px] sm:h-[72px] top-0 left-0 absolute flex justify-between items-center p-4 z-10 shadow-md backdrop-blur border-b border-b-[#bdc3c780] lg:hidden">
+    <div className="w-full h-[48px] sm:h-[72px] top-0 left-0 absolute flex justify-between items-center p-4 z-50 shadow-md backdrop-blur border-b border-b-[#bdc3c780] lg:hidden">
       <IoIosSearch className="text-[24px] sm:text-[28px]" />
-      <Link href={"/"}>
-        <div className="absolute top-0 left-0 w-full h-[48px] sm:h-[72px] flex justify-center items-center">
+      <div className="absolute top-0 left-0 w-full z-[0] h-[48px] sm:h-[72px] flex justify-center items-center">
+        <Link href={"/"} className="w-fit">
           <Image
             src="/images/logo.png"
             alt="logo"
@@ -18,8 +55,8 @@ const NavMobile = () => {
             height={28}
             className="cursor-pointer w-[28px] h-[28px] sm:w-[46px] sm:h-[46px]"
           />
-        </div>
-      </Link>
+        </Link>
+      </div>
       <ul className="list-none flex justify-center items-center text-[20px] sm:text-[24px] gap-[2px] sm:gap-2">
         <li className="relative inline-block w-fit px-2 xsm:text-[14px] sm:text-[16px] md:text-[22px] font-medium text-[#C7C7C7] cursor-pointer">
           <IoLanguageSharp className="absolute top-1.5 left-3 pl-1" />
@@ -33,11 +70,16 @@ const NavMobile = () => {
           </select>
           <TiArrowSortedDown className="absolute top-1.5 right-3 pr-1 pointer-events-none" />
         </li>
-        <li>|</li>
-        <li className="h-[22px] sm:h-[28px] flex justify-center items-center space-x-1 px-2 text-[#4A90E2] border-[#4A90E2] border rounded-xl bg-[#1456a44d] cursor-pointer hover:bg-[#1456a499]">
+        <li className="px-1">|</li>
+        <li
+          className="h-[22px] sm:h-[28px] flex justify-center z-[1] items-center space-x-1 px-2 text-[#4A90E2] border-[#4A90E2] border rounded-xl bg-[#1456a44d] cursor-pointer hover:bg-[#1456a499]"
+          onClick={onClick}
+        >
           <FaWandMagicSparkles />
         </li>
       </ul>
+
+      <Modal modal={modal} setModal={setModal} btnId={buttonId} />
     </div>
   );
 };
