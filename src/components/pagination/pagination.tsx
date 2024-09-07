1c4524cb32a7b4cabf2d9acd8cddd868976ac97c
DMaximoVarela <maxivarela95@gmail.com>
2024-09-07 19:19:23 -0300

    feat: add the logic of the pagination

--- src/components/pagination/pagination.tsx
+++ src/components/pagination/pagination.tsx
@@ -1,25 +1,101 @@
+import useButtonsStore from "@/stores/buttonsStore";
+import usePaginationStore from "@/stores/paginationStore";
+import { paginationButtons } from "@/utils/paginationButtons";
 import { IoIosArrowForward, IoIosArrowBack } from "react-icons/io";
 
 const Pagination = () => {
+  const activePage = usePaginationStore((state) => state.active);
+  const setActivePage = usePaginationStore((state) => state.setActive);
+  const buttons = useButtonsStore((state) => state.buttons);
+  const pageButtons = paginationButtons(buttons);
+
+  const totalPages = pageButtons.length;
+
+  const getPaginationRange = () => {
+    const range: (string | number)[] = [];
+
+    range.push(0);
+
+    if (activePage > 3) {
+      range.push("...");
+    }
+
+    for (
+      let i = Math.max(1, activePage - 2);
+      i <= Math.min(totalPages - 2, activePage + 2);
+      i++
+    ) {
+      range.push(i);
+    }
+
+    if (activePage < totalPages - 4) {
+      range.push("...");
+    }
+
+    range.push(totalPages - 1);
+
+    return range;
+  };
+
+  const paginationRange = getPaginationRange();
+
   return (
     <div className="flex justify-center items-center space-x-4 text-[16px] text-white bg-[#00000059] w-fit p-2 rounded-lg backdrop-blur-sm">
-      <IoIosArrowBack className="hover:bg-[#4d4d4d40] cursor-pointer rounded-md p-2 size-full" />
+      <IoIosArrowBack
+        className={`${
+          activePage === 0
+            ? "text-slate-400 cursor-not-allowed"
+            : "hover:bg-[#4d4d4d40] cursor-pointer"
+        } rounded-md p-2 size-full`}
+        onClick={() => {
+          if (activePage > 0) {
+            setActivePage(activePage - 1);
+          }
+        }}
+      />
       <ul className="flex justify-center items-center space-x-2">
-        <li className="p-2 bg-[#2072D3] hover:bg-[#1456A4] cursor-pointer rounded-md">
-          1
-        </li>
-        <li className="p-2 hover:bg-[#4d4d4d40] cursor-pointer rounded-md">
-          2
-        </li>
-        <li className="p-2 hover:bg-[#4d4d4d40] cursor-pointer rounded-md">
-          3
-        </li>
-        <li className="p-2 pointer-events-none">...</li>
-        <li className="p-2 hover:bg-[#4d4d4d40] cursor-pointer rounded-md">
-          16
-        </li>
+        {paginationRange.map((page, index) => {
+          if (typeof page === "string") {
+            return (
+              <li
+                key={index}
+                className="p-2 text-slate-400 cursor-not-allowed"
+                style={{ pointerEvents: "none" }}
+              >
+                {page}
+              </li>
+            );
+          }
+
+          return (
+            <li
+              key={index}
+              className={`p-2 ${
+                activePage === page
+                  ? "bg-[#2072D3] hover:bg-[#1456A4]"
+                  : "hover:bg-[#4d4d4d40]"
+              } cursor-pointer rounded-md`}
+              onClick={() => {
+                setActivePage(page);
+              }}
+            >
+              {page + 1}
+            </li>
+          );
+        })}
       </ul>
-      <IoIosArrowForward className="hover:bg-[#4d4d4d40] cursor-pointer rounded-md p-2 size-full" />
+      <IoIosArrowForward
+        className={`${
+          activePage === totalPages - 1
+            ? "text-slate-400 cursor-not-allowed"
+            : "hover:bg-[#4d4d4d40] cursor-pointer"
+        } rounded-md p-2 size-full`}
+        onClick={() => {
+          if (activePage < totalPages - 1) {
+            setActivePage(activePage + 1);
+          }
+        }}
+      />
     </div>
   );
 };
