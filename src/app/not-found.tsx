d7a5e7845aacacf3bff4169321bb5049890d0082
DMaximoVarela <maxivarela95@gmail.com>
2024-09-07 13:47:49 -0300

    feat: add the translations to error404 page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -2,17 +2,20 @@ import { FaArrowLeft } from "react-icons/fa";
 import Image from "next/image";
 import Link from "next/link";
 import style from "./not-found.module.css";
+import { useTranslations } from "next-intl";
 
 export default function NotFound() {
+  const t = useTranslations("error404page")
+
   return (
     <div className="flex justify-between items-center px-6 md:space-x-7">
       <div className={style.bg}></div>
       <div className="text-start">
         <h1 className="text-[18px] sm:text-[32px] lg:text-[36px] 2xl:text-[60px] 4xl:text-[72px] text-[#e8e8e8] font-bold">
-          ¡<span className="text-[#4A90E2]">Oops</span>, página
-          <span className="text-[#4A90E2]"> no</span>
+          ¡<span className="text-[#4A90E2]">Oops</span>, {t("title.lineOne")}
+          <span className="text-[#4A90E2]"> {t("title.lineTwo")}</span>
           <br />
-          encontrada!
+          {t("title.lineThree")}
         </h1>
         <Link
           href={"/inicio"}
@@ -20,7 +23,7 @@ export default function NotFound() {
         >
           <FaArrowLeft className="text-[14px] sm:text-[16px] xl:text-[20px] 2xl:text-[24px] 4xl:text-[28px]" />
           <h2 className="text-[14px] sm:text-[20px] xl:text-[24px] 2xl:text-[32px] 4xl:text-[36px] font-normal">
-            Volver al Inicio
+            {t("btnText")}
           </h2>
         </Link>
       </div>
