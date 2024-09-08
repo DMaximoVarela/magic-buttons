e7c2e9cb2b1992b61b62e7dccb956d27982ae04e
DMaximoVarela <maxivarela95@gmail.com>
2024-09-08 20:30:03 -0300

    feat: add the mini skeleton component

---
+++ src/components/Skeletons/miniContainerSkeleton/miniContainerSkeleton.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const MiniContainerSkeleton = () => {
+  return (
+    <div>
+      <button className="w-24 h-6 rounded-md animate-pulse bg-slate-600" />
+    </div>
+  );
+};
+
+export default MiniContainerSkeleton;
