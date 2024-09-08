389184f9ec1a7a9d04af5be07c248c95d0a7f575
DMaximoVarela <maxivarela95@gmail.com>
2024-09-08 20:22:48 -0300

    feat: add the buttonContainerSkeleton

--- src/components/cards/cards.tsx
+++ src/components/cards/cards.tsx
@@ -6,11 +6,14 @@ import { useEffect, useState } from "react";
 import useButtonsStore from "@/stores/buttonsStore";
 import { paginationButtons } from "@/utils/paginationButtons";
 import usePaginationStore from "@/stores/paginationStore";
+import ButtonContainerSkeleton from "../Skeletons/buttonContainerSkeleton/buttonContainerSkeleton";
 
 const Cards = () => {
   const filters = useFiltersStore((state) => state.filters);
   const getButtons = useButtonsStore((state) => state.getButtons);
+  const getAllButtons = useButtonsStore((state) => state.getAllButtons);
   const allButtons = useButtonsStore((state) => state.buttons);
+  const allButtonsCopy = useButtonsStore((state) => state.buttonsCopy);
   const activePage = usePaginationStore((state) => state.active);
   const pageButtons = paginationButtons(allButtons);
 
@@ -30,17 +33,24 @@ const Cards = () => {
       }
 
       await getButtons(category, tamano, estilo);
+      await getAllButtons();
     };
 
     fetchData();
   }, [filters]);
 
+  const numberedArrayAux = [1, 2, 3, 4, 5, 6];
+
   return (
     <div>
       <ul className="flex flex-wrap justify-center sm:justify-between items-center gap-y-6 lg:gap-y-10">
-        {pageButtons[activePage]?.map((btn) => (
-          <ButtonContainer key={btn.id} btnId={btn.id} />
-        ))}
+        {allButtonsCopy.length > 0
+          ? pageButtons[activePage]?.map((btn) => (
+              <ButtonContainer key={btn.id} btnId={btn.id} />
+            ))
+          : numberedArrayAux.map((value) => (
+              <ButtonContainerSkeleton key={value} />
+            ))}
       </ul>
     </div>
   );
