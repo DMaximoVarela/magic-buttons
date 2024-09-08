dfb77d78ab8d390e38953598d5ec5d76cb33bc3b
DMaximoVarela <maxivarela95@gmail.com>
2024-09-08 20:25:09 -0300

    fix: delete the cursor pointer

--- src/components/Skeletons/buttonContainerSkeleton/buttonContainerSkeleton.tsx
+++ src/components/Skeletons/buttonContainerSkeleton/buttonContainerSkeleton.tsx
@@ -3,10 +3,10 @@ const ButtonContainerSkeleton = () => {
     <>
       <div className="relative w-[250px] lg:w-[350px] 3xl:w-[450px] h-[200px] sm:h-[250px] 2xl:h-[350px] rounded-2xl bg-[#191919] flex flex-col justify-center items-center p-4">
         <div className="absolute top-0 right-0 m-2">
-          <button className="w-[90px] sm:w-[100px] 3xl:w-[110px] h-6 p-1 rounded-lg bg-slate-600 animate-pulse cursor-pointer" />
+          <button className="w-[90px] sm:w-[100px] 3xl:w-[110px] h-6 p-1 rounded-lg bg-slate-600 animate-pulse" />
         </div>
 
-        <div className="w-fit h-fit mb-4 cursor-pointer">
+        <div className="w-fit h-fit mb-4">
           <button className="w-32 h-12 rounded-lg animate-pulse bg-slate-600" />
         </div>
 
