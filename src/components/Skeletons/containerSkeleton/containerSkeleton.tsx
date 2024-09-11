a9f688839531d292618bd16ddd6375cb941f9622
DMaximoVarela <maxivarela95@gmail.com>
2024-09-11 07:46:10 -0300

    style: change the skeleton color

--- src/components/Skeletons/containerSkeleton/containerSkeleton.tsx
+++ src/components/Skeletons/containerSkeleton/containerSkeleton.tsx
@@ -1,7 +1,7 @@
 const ContainerSkeleton = () => {
   return (
     <div>
-      <button className="w-40 h-12 animate-pulse bg-slate-600 p-2 rounded-lg cursor-default" />
+      <button className="w-40 h-12 animate-pulse bg-[#3f3f3f] p-2 rounded-lg cursor-default" />
     </div>
   );
 };
--- src/components/Skeletons/miniContainerSkeleton/miniContainerSkeleton.tsx
+++ src/components/Skeletons/miniContainerSkeleton/miniContainerSkeleton.tsx
@@ -1,7 +1,7 @@
 const MiniContainerSkeleton = () => {
   return (
     <div>
-      <button className="w-24 h-6 rounded-md animate-pulse bg-slate-600 cursor-default" />
+      <button className="w-24 h-6 rounded-md animate-pulse bg-[#3f3f3f] cursor-default" />
     </div>
   );
 };
